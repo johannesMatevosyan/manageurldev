3ddfa932fb0bd8a955a20e2b8731139304cd7b32
hayk <>
2014-05-19 20:13:18 +0400

    haykkyureghyan

--- application/views/pages/data_table.php
+++ application/views/pages/data_table.php
@@ -2,3 +2,33 @@
 
 
 ?>
+<table class="table zebra-style">
+    <thead>
+      <tr>
+        <th>#</th>
+        <th>First Name</th>
+        <th>Last Name</th>
+        <th>City</th>
+      </tr>
+    </thead>
+    <tbody>
+      <tr class="odd">
+        <td>1</td>
+        <td id="first_name:1" contenteditable="true">Karthikeyan</td>
+        <td id="last_name:1" contenteditable="true">K</td>
+        <td id="city:1" contenteditable="true">Chennai</td>
+      </tr>
+      <tr>
+        <td>2</td>
+        <td id="first_name:2" contenteditable="true">Facebook</td>
+        <td id="first_name:2" contenteditable="true">Inc</td>
+        <td id="first_name:2" contenteditable="true">California</td>
+      </tr>
+      <tr class="odd">
+        <td>3</td>
+        <td id="first_name:3" contenteditable="true">W3lessons</td>
+        <td id="first_name:3" contenteditable="true">Blog</td>
+        <td id="first_name:3" contenteditable="true">Chennai, India</td>
+      </tr>
+    </tbody>
+ </table>

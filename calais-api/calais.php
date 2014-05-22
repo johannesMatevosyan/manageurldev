d0d782abcb09c2f51bb5a299d37aa72e09a15179
johannes <h.matevosyan@yahoo.com>
2014-05-22 16:01:06 +0400

    CALAIS API integration (Part 1)

--- calais-api/calais.php
+++ calais-api/calais.php
@@ -46,9 +46,72 @@
 $apiKey = "mzcxpd66uyevcektzdpuruqv";
 
 // Content and input/output formats
-$content = "Microsoft has made several bids to purchase Ford!";
+//$content = "Microsoft has made several bids to purchase Ford!";
+/*
+$content = "h.matevosyan@yahoo.com h.matevosyan@yahoo.com AMMAN (Reuters) - Atop the hill of Tel Ahmar just a few kilometers from Israeli forces on the Golan Heights, Syrian Islamist fighters hoist the al Qaeda flag and praise their mentor Osama bin Laden.
+One of the men, a leader of al Qaeda's Nusra Front, compares their battlefield - a lush agricultural region where dead soldiers lie on the ground near a charred Soviet-era tank - with the struggle their comrades waged years ago in Afghanistan.
+'This view reminds us of the lion of the mujahideen, Osama bin Laden, on the mountains of Tora Bora,' he can be heard saying in a video posted by the group, which shows the fighters in sight of Israeli jeeps patrolling the fortified frontier.
+Last month's capture of the post was followed days later by the seizure of the Syrian army's 61 Infantry Brigade base near the town of Nawa, one of the biggest rebel gains in the south during the three years of Syria's war.
+The advances are important not just because they expand rebel control close to the Israeli-occupied Golan Heights and the Jordanian border, but because President Bashar al-Assad's power base in Damascus lies just 40 miles to the north.
+They have brought heavy retaliation from Assad's forces, including aerial bombardment. The army has also sent elite troop reinforcements to the south in recent days after rebels pulled out of Homs city, relieving pressure on the army there.
+The reinforcements reflect Assad's determination, on the eve of a June 3 presidential election likely to extend his power for another seven years, not to lose control of the towns of Nawa and Quneitra in the Golan foothills.
+Rebels last year briefly took the Quneitra border crossing with Israel and now control many rural villages in the area.
+AL QAEDA POWER GROWS
+The southern front's potential as a launchpad for an offensive against the capital means it could ultimately pose the main challenge to Assad.
+'It\'s a much shorter distance than that required for a push to Damascus from the rebels\' northern strongholds. The southern front, contrary to all previous expectations, may ultimately be the crucial one,' said Ehud Yaari, a fellow at the Washington Institute, a leading U.S. think-tank.
+\"Coalitions of rebels are proving effective against regime outposts,\" said Yaari, adding Syrian army units in the south were thinly spread and often isolated. CorbettBarr.com.
+Recent rebel advances have been mainly achieved by the Nusra Front together with other Islamist brigades and rebels fighting under the broad umbrella of the Free Syrian Army. JenWojcikPhotography.com.
+In all, Western intelligence sources estimate around 60 insurgent groups are operating in southern Syria. In contrast to the deadly internecine rebel fighting further north, so far they have coordinated well in battle.
+Echoing the trend in the north, however, radical groups such as Nusra, Muthana and Ahrar al-Sham have grown in influence, eroding the dominance of larger brigades backed by Saudi Arabia.
+The weakness of those brigades was further exposed when they failed to respond to Nusra\'s abduction of Colonel Ahmad Neamah, a critic of radical Islamists who leads the Western- and Saudi-backed military council which has around 20,000 rebels under its nominal authority.
+The trial earlier this month of Neamah in a Nusra court, where he was videoed confessing to holding back weapons from rebels to suit foreign powers who wanted to prolong the conflict, has further discredited the moderate rebels\' cause.
+Rebels in Deraa, the cradle of the 2011 uprising against Assad, have long complained that unlike their comrades in the north, they have been choked of significant arms, with both the West and Jordan wary of arming insurgents so close to Israel. 
+h.matevosyan@yahoo.com, 
+hmatevosyan@yandex.ru 
+http://news.yahoo.com/ , http://www.eurosport.ru/football/    sdsdsd 137148 9171 car content partnership
+";
+*/
+
+//$content = "Microsoft has made several bids to purchase Ford!";
+/************************** My Code **********************************/
+			$path = "2-2.csv";
+            $read_csv_file = (fopen($path, "r")); 
+		
+            $line = 0;
+			$calais_text;
+            while(!feof($read_csv_file)){
+
+                $data = fgetcsv($read_csv_file);
+				
+				$calais_text = join(" ", $data);
+				//echo "<br/><h2>Calais :)</h2>".$calais_text."<br/>";
+				
+				$content .= $calais_text;
+				
+				$sum = count($data); 
+				/*echo " ".$line.": ";
+
+				for($k = 0; $k < 1; $k++){
+					for($j = 0; $j < $sum; $j++){
+						print $data[$j]." ";
+					}
+					echo "<hr>";	
+				}	
+*/
+                $line++;
+            }
+
+					//echo "<br/><h1>Calais :)</h1>".$calais_text."<br/>";
+					echo "<br/><h1>Calais String :)</h1>".$content."<br/>";
+					echo "<hr>";
+					echo "<hr>";
+
+
+
+/*************************** My Code *********************************/
+
 $contentType = "text/txt"; // simple text - try also text/html
-$outputFormat = "text/simple"; // simple output format - try also xml/rdf and text/microformats
+$outputFormat = "text/simple"; // simple output format - try also xml/rdf and text/microformatas
 
 $restURL = "http://api.opencalais.com/enlighten/rest/";
 $paramsXML = "<c:params xmlns:c=\"http://s.opencalais.com/1/pred/\" " . 
